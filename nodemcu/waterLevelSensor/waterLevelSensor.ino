d6507ecc79dfbab78db9bfa131a1d9d502b594b1
moonblade <moonblade168@gmail.com>
2021-09-16 09:33:12 +0530

    Add auto update code to sensor as well

--- nodemcu/waterLevelSensor/waterLevelSensor.ino
+++ nodemcu/waterLevelSensor/waterLevelSensor.ino
@@ -1,15 +1,20 @@
 #include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+
 
 #define FIREBASE_HOST "water-level-indicator-a555e-default-rtdb.firebaseio.com"  //Change to your Firebase RTDB project ID e.g. Your_Project_ID.firebaseio.com
 #define FIREBASE_AUTH "RSycUEGVNj1wiOVGrmXjQkdpE65voJNJmaGPs3Z7" //Change to your Firebase RTDB secret password
 #define WIFI_SSID "sarayi_tf_2.4"
-/* #define WIFI_SSID "sarayi_2" */
 #define WIFI_PASSWORD "code||die"
 #define VCC D0
 #define TRIGGERPIN D1
 #define ECHOPIN    D2
 #define GND D3
+
+const String CURRENT_VERSION = String("__BINARY_NAME:waterLevelSensor__BINARY_VERSION:") + __DATE__ + __TIME__ + "__";
+String latestVersion, downloadUrl;
 
 //Define Firebase Data objects
 FirebaseData waterLevelData;
@@ -54,6 +59,36 @@ void setup() {
   Firebase.reconnectWiFi(true);
 }
 
+void updateFirmware(String firmwareUrl) {
+  WiFiClientSecure client;
+  client.setInsecure();
+  t_httpUpdate_return ret = ESPhttpUpdate.update(client, firmwareUrl); 
+  switch(ret) {
+      case HTTP_UPDATE_FAILED:
+          Serial.println("[update] Update failed.");
+          break;
+      case HTTP_UPDATE_NO_UPDATES:
+          Serial.println("[update] Update no Update.");
+          break;
+      case HTTP_UPDATE_OK:
+          Serial.println("[update] Update ok."); // may not be called since we reboot the ESP
+          break;
+    }
+  }
+
+void checkForUpdates() {
+  Firebase.set(waterLevelData, "/waterLevelSensor/firmware/currentVersion", CURRENT_VERSION);
+  Firebase.getString(waterLevelData, "/waterLevelSensor/firmware/binaryVersion");
+  latestVersion = waterLevelData.stringData();
+
+  if (!latestVersion.equalsIgnoreCase(CURRENT_VERSION)) {
+    Serial.println("Version mismatch, downloading update");
+    Firebase.getString(waterLevelData, "/waterLevelSensor/firmware/downloadUrl");
+    downloadUrl = waterLevelData.stringData();
+    updateFirmware(downloadUrl);
+  }
+}
+
 void loop() {
   digitalWrite(GND, LOW);
   digitalWrite(VCC, HIGH);
@@ -62,5 +97,6 @@ void loop() {
 
   Firebase.set(waterLevelData, path + "/measurement", dist);
   Firebase.setTimestamp(waterLevelData, path + "/timestamp");
+  checkForUpdates();
   delay(10000);
 }
